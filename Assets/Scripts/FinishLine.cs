5bb7f0cde5db7ea84de80d7519e2d668bb038a8b
bramblestars <txxcui@gmail.com>
2023-07-18 15:17:05 -0500

    add particle effect when crossing finish line

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Penguin") {
             Debug.Log("You win!");
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
