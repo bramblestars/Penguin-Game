18251b6e89d60ce9d5acd278bc524599b46f7b63
bramblestars <txxcui@gmail.com>
2023-07-18 02:30:53 -0500

    add script for spikes and possible future collision

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] float loadDelay = 0.5f;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Spikes") {
+            Invoke("ReloadScene", loadDelay);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
