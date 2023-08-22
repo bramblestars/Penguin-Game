c1171f0d48086be5e1c7af21e9b9d590b7a7a67c
bramblestars <txxcui@gmail.com>
2023-08-22 00:51:59 -0500

    add clouds back

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [SerializeField] float baseSpeed = 0.4f;
+    [SerializeField] float randomScale = 0.1f;
+
+    private float randomSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpeed = Random.Range(baseSpeed, baseSpeed + randomScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        transform.position += Vector3.right * randomSpeed;
+    }
+}
